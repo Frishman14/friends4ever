4ee8e81c99482525f40d4792f02b24bcf8a4cd55
Itay <Frishman.itay@gmail.com>
2020-03-21 22:30:02 +0200

    using rest_fraework generics - concisegit add -A

--- f4e_site/animals/views.py
+++ f4e_site/animals/views.py
@@ -1,31 +1,13 @@
-from rest_framework import mixins
-from rest_framework import generics
-from django.http import Http404
 from .models import Snippet
 from .serializers import SnippetSerializer
+from rest_framework import generics
 
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class SnippetList(generics.ListAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, requset, *args, **kwargs):
-        return self.list(requset, *args, **kwargs)
 
-    def post(self, requset, *args, **kwargs):
-        return self.create(requset, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.ListAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
