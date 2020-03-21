f31d200538e485d9502910db688b07c2ce1e0cdf
Itay <Frishman.itay@gmail.com>
2020-03-21 22:12:01 +0200

    updeate with API-CLASSES

--- f4e_site/animals/urls.py
+++ f4e_site/animals/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('animals/', views.snippet_list),
-    path('animals/<int:pk>/', views.snippet_detail),
+    path('animals/', views.SnippetList.as_view()),
+    path('animals/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
